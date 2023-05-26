a2879ed74e2c6476b6467b6132a0c078aefbb207
Serg <skopchalyuk@gmail.com>
2023-05-26 15:59:45 +0200

    reinitialized td submodule

--- build-gyp.sh
+++ build-gyp.sh
@@ -15,16 +15,19 @@ rm -rf prebuilds/tdlib/
 cd td/
 git pull
 
-brew install gperf openssl zlib
-#sudo apt-get install gperf openssl zlib1g-dev gcc g++ musl-dev make cmake binutils -y
+brew install gperf openssl zlib #macos-only
+
+#see https://tdlib.github.io/td/build.html?language=JavaScript
+#sudo sudo apt-get install make git zlib1g-dev libssl-dev gperf php-cli cmake g++ -y
 cmake --version
 
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
--DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl@1.1 \
--DZLIB_INCLUDE_DIR=/opt/homebrew/opt/zlib/include \
--DZLIB_LIBRARY=/opt/homebrew/opt/zlib/lib/libz.a \
--DOPENSSL_USE_STATIC_LIBS=TRUE -DZLIB_USE_STATIC_LIBS=TRUE ..
+#-DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl@1.1 \ macos-only
+#-DZLIB_INCLUDE_DIR=/opt/homebrew/opt/zlib/include \ macos-only
+#-DZLIB_LIBRARY=/opt/homebrew/opt/zlib/lib/libz.a \ macos-only
+#-DOPENSSL_USE_STATIC_LIBS=TRUE -DZLIB_USE_STATIC_LIBS=TRUE \ macos-only
+..
 cmake --build . --target tdjson -- -j 3
 cd ..
 
