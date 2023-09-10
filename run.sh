6dd90deace6368b384c0349755fdf6f37789df9f
Serg <skopchalyuk@gmail.com>
2023-09-10 19:57:25 +0200

    works on n8n=1.4.0 with prebuilt packages

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cd ~/.n8n/
 mkdir -p nodes && cd nodes
 npm link n8n-nodes-tdlib
 
-npm config set registry http://0.0.0.0:4873/
+#npm config set registry http://0.0.0.0:4873/
 
 DEBUG=tdl,telepilot-cred,telepilot-node,telepilot-trigger,telepilot-cm EXECUTIONS_PROCESS=main N8N_LOG_LEVEL=debug n8n start
