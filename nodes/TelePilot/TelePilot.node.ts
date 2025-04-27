telepilotco/n8n-nodes-telepilot

commit 4ec2a14203ca462b6231124b245e10f2be87dc2e

Add getMessageLink func (#15)

nodes/TelePilot/TelePilot.node.ts
@@ -538,6 +538,15 @@ export class TelePilot implements INodeType {
 						message_id,
 					});
 					returnData.push(result);
+				} else if (operation === 'getMessageLink') {
+					const chat_id = this.getNodeParameter('chat_id', 0) as string;
+					const message_id = this.getNodeParameter('message_id', 0) as string;
+					const result = await client.invoke({
+						_: 'getMessageLink',
+						chat_id,
+						message_id,
+					});
+					returnData.push(result);
 				} else if (operation === 'sendMessage') {
 					const chat_id = this.getNodeParameter('chat_id', 0) as string;
 					const messageText = this.getNodeParameter('messageText', 0) as string;

nodes/TelePilot/common.descriptions.ts
@@ -358,6 +358,11 @@ export const operationMessage: INodeProperties = {
 			value: 'sendMessage',
 			action: 'Send text message',
 		},
+		{
+			name: 'Get Message Link',
+			value: 'getMessageLink',
+			action: 'Get direct link to a specific message in a group or supergroup',
+		},
 	],
 default: 'sendMessage',
 	noDataExpression: true,
@@ -449,6 +454,7 @@ export const variable_chat_id: INodeProperties = {
 			'deleteChat',
 			'addChatMembers',
 			'sendChatAction',
+			'getMessageLink',
 		],
 			resource: ['chat', 'message'],
 	},
@@ -542,7 +548,7 @@ export const variable_message_id: INodeProperties = {
 	required: true,
 	displayOptions: {
 	show: {
-		operation: ['getMessage', 'editMessageText'],
+		operation: ['getMessage', 'editMessageText', 'getMessageLink'],
 			resource: ['message'],
 	},
 },

commit ce7bad08ae2fe142385f9f38f62026b1766fd93d

Added message_thread_id parameter to all sendMessage actions (#18)
 - Adresses https://github.com/telepilotco/n8n-nodes-telepilot/issues/8 by adding `message_thread_id` to every action that is using `sendMessage`
 - Fixed eslit issues introduced in https://github.com/telepilotco/n8n-nodes-telepilot/pull/15
 - package.version = `0.5.1`

nodes/TelePilot/TelePilot.node.ts
@@ -37,6 +37,7 @@ import {
 	variable_query,
 	variable_remote_file_id,
 	variable_reply_to_msg_id,
+	variable_message_thread_id,
 	variable_revoke,
 	variable_supergroup_id,
 	variable_title,
@@ -97,7 +98,7 @@ export class TelePilot implements INodeType {
 			//Chat
 			variable_chat_id,
 			variable_from_chat_id,
-			//Chat
+			//Message
 			variable_is_marked_as_unread,
 			variable_from_message_id,
 			variable_limit,
@@ -118,6 +119,7 @@ export class TelePilot implements INodeType {
 			variable_username,
 			variable_query,
 			variable_title,
+			variable_message_thread_id,
 			variable_description,
 			variable_is_channel,
 			variable_user_ids,
@@ -551,10 +553,12 @@ export class TelePilot implements INodeType {
 					const chat_id = this.getNodeParameter('chat_id', 0) as string;
 					const messageText = this.getNodeParameter('messageText', 0) as string;
 					const reply_to_msg_id = this.getNodeParameter('reply_to_msg_id', 0) as string;
+					const message_thread_id = this.getNodeParameter('message_thread_id', 0) as number;
 					const result = await client.invoke({
 						_: 'sendMessage',
 						chat_id,
 						reply_to_msg_id,
+						message_thread_id,
 						input_message_content: {
 							_: 'inputMessageText',
 							text: {
@@ -569,6 +573,7 @@ export class TelePilot implements INodeType {
 					const videoFilePath = this.getNodeParameter('videoFilePath', 0) as string;
 					let videoCaption: string | null = this.getNodeParameter('fileCaption', 0) as string;
 					const reply_to_msg_id = this.getNodeParameter('reply_to_msg_id', 0) as string;
+					const message_thread_id = this.getNodeParameter('message_thread_id', 0) as number;
 
 					if (videoCaption === '' && videoCaption.length == 0) {
 						videoCaption = null;
@@ -578,6 +583,7 @@ export class TelePilot implements INodeType {
 						_: 'sendMessage',
 						chat_id,
 						reply_to_msg_id,
+						message_thread_id,
 						input_message_content: {
 							_: 'inputMessageVideo',
 							video: {
@@ -813,6 +819,7 @@ export class TelePilot implements INodeType {
 					const filePath = this.getNodeParameter('filePath', 0) as string;
 					let fileCaption: string | null = this.getNodeParameter('fileCaption', 0) as string;
 					const reply_to_msg_id = this.getNodeParameter('reply_to_msg_id', 0) as string;
+					const message_thread_id = this.getNodeParameter('message_thread_id', 0) as number;
 
 					if (fileCaption === '' && fileCaption.length == 0) {
 						fileCaption = null;
@@ -821,6 +828,7 @@ export class TelePilot implements INodeType {
 						_: 'sendMessage',
 						chat_id,
 						reply_to_msg_id,
+						message_thread_id,
 						input_message_content: {
 							_: 'inputMessageDocument',
 							document: {
@@ -839,6 +847,7 @@ export class TelePilot implements INodeType {
 					const localFilePath = this.getNodeParameter('localFilePath', 0) as string;
 					let photoCaption: string | null = this.getNodeParameter('photoCaption', 0) as string;
 					const reply_to_msg_id = this.getNodeParameter('reply_to_msg_id', 0) as string;
+					const message_thread_id = this.getNodeParameter('message_thread_id', 0) as number;
 
 					if (photoCaption === '' && photoCaption.length == 0) {
 						photoCaption = null;
@@ -847,6 +856,7 @@ export class TelePilot implements INodeType {
 						_: 'sendMessage',
 						chat_id,
 						reply_to_msg_id,
+						message_thread_id,
 						input_message_content: {
 							_: 'inputMessagePhoto',
 							photo: {
@@ -898,19 +908,21 @@ export class TelePilot implements INodeType {
 					const from_chat_id = this.getNodeParameter('from_chat_id', 0) as string;
 
 					const message_ids: string = this.getNodeParameter('message_ids', 0) as string;
-					debug(message_ids);
+					const message_thread_id = this.getNodeParameter('message_thread_id', 0) as number;
+
 					const idsArray = message_ids
 						.toString()
 						.split(',')
 						.map((s) => s.toString().trim())
 						.filter((s) => s.length > 0);
-					debug(idsArray);
+
 
 					const result = await client.invoke({
 						_: 'forwardMessages',
 						chat_id,
 						from_chat_id,
 						message_ids: idsArray,
+						message_thread_id,
 					});
 					returnData.push(result);
 				}

nodes/TelePilot/common.descriptions.ts
@@ -329,6 +329,11 @@ export const operationMessage: INodeProperties = {
 			action: 'Forward messages',
 		},
 		{
+			name: 'Get Message Link',
+			value: 'getMessageLink',
+			action: 'Get direct link to a specific message in a group or supergroup',
+		},
+		{
 			name: 'Get Messages',
 			value: 'getMessage',
 			action: 'Get message',
@@ -357,12 +362,7 @@ export const operationMessage: INodeProperties = {
 			name: 'Send Text Message',
 			value: 'sendMessage',
 			action: 'Send text message',
-		},
-		{
-			name: 'Get Message Link',
-			value: 'getMessageLink',
-			action: 'Get direct link to a specific message in a group or supergroup',
-		},
+		}
 	],
 default: 'sendMessage',
 	noDataExpression: true,
@@ -707,6 +707,21 @@ export const variable_reply_to_msg_id: INodeProperties = {
 	default: '',
 	placeholder: 'Text',
 	description: 'Identifier of Message',
+};
+
+export const variable_message_thread_id: INodeProperties = {
+	displayName: 'Reply to threadID',
+	name: 'message_thread_id',
+	type: 'number',
+	displayOptions: {
+		show: {
+			operation: ['sendMessage', 'sendMessagePhoto', 'forwardMessages', 'sendMessageFile', 'sendMessageVideo'],
+			resource: ['message'],
+		},
+	},
+	default: '',
+	placeholder: 'Text',
+	description: 'If not 0, a message thread identifier in which the message will be sent',
 };
 
 export const variable_supergroup_id: INodeProperties = {